eb00dd532d9e1c4c306862a507e9f371d8a672dd
CN 8 <administrator@CN-8.lan>
2019-03-27 13:40:08 +0000

    finishing touches

--- app.js
+++ app.js
@@ -1,66 +1,64 @@
-const holes = document.querySelectorAll('.hole');
-const scoreBoard = document.querySelector('.score');
-const moles = document.querySelectorAll('.mole')
+const holes = document.querySelectorAll(".hole");
+const scoreBoard = document.querySelector(".score");
+const moles = document.querySelectorAll(".mole");
 const btn = document.getElementById("startBtn");
 let lastHole;
 let timeUp = false;
 let score = 0;
-let whack = new Audio('./sounds/whack.wav');
-let slap = new Audio('./sounds/slap.wav');
-let clink = new Audio('./sounds/impact.wav');
-let hitsound = new Audio('./sounds/hit.wav');
+let whack = new Audio("./sounds/whack.wav");
+let slap = new Audio("./sounds/slap.wav");
 
 function randTime(min, max) {
-    return Math.round(Math.random() * (max - min)) + min;
+  return Math.round(Math.random() * (max - min)) + min;
 }
 
-const randHole = (holes) => {
-    const idx = Math.floor(Math.random() * holes.length);
-    const hole = holes[idx];
-    console.log(hole)
-    if(hole === lastHole) {
-        console.log("Reroll.");
-        return randHole(holes);
-    }
-    lastHole = hole;
-    return hole;
-}
+const randHole = holes => {
+  const idx = Math.floor(Math.random() * holes.length);
+  const hole = holes[idx];
+  console.log(hole);
+  if (hole === lastHole) {
+    console.log("Reroll.");
+    return randHole(holes);
+  }
+  lastHole = hole;
+  return hole;
+};
 
 function peep() {
-    const time = randTime(500, 2000);
-    const hole = randHole(holes);
-    hole.classList.add('up');
-    setTimeout(() => {
-        hole.classList.remove('up');
-        if(!timeUp) peep();
-    }, time);
+  const time = randTime(500, 2000);
+  const hole = randHole(holes);
+  hole.classList.add("up");
+  setTimeout(() => {
+    hole.classList.remove("up");
+    if (!timeUp) peep();
+  }, time);
 }
 
 function startGame() {
-    btn.disabled = true;
-    music = new Audio('./sounds/ambience.wav');
-    music.play();
-    music.volume = 0.1
-    scoreBoard.textContent = 0;
-    timeUp = false;
-    score = 0;
-    peep();
-    let cheering = new Audio('./sounds/cheering.wav');
-    setTimeout(() => timeUp = true, 15000);
-    setTimeout(() => cheering.play(), 15000)
-    setTimeout(() => btn.disabled = false, 15000)
+  btn.disabled = true;
+  music = new Audio("./sounds/ambience.wav");
+  music.play();
+  music.volume = 0.1;
+  scoreBoard.textContent = 0;
+  timeUp = false;
+  score = 0;
+  peep();
+  let cheering = new Audio("./sounds/cheering.wav");
+  setTimeout(() => (timeUp = true), 15000);
+  setTimeout(() => cheering.play(), 15000);
+  setTimeout(() => (btn.disabled = false), 15000);
 }
 
 function hit(e) {
-    console.log
-    if(!e.isTrusted) return; // cheater!
-    score++;
-    this.parentNode.classList.remove('up');
-    scoreBoard.textContent = score;
-    let sounds = [whack, slap, hitsound, clink];
-    let randSound = sounds[Math.floor(Math.random()*sounds.length )];
-    randSound.load()
-    randSound.play()
+  console.log;
+  if (!e.isTrusted) return; // cheater!
+  score++;
+  this.parentNode.classList.remove("up");
+  scoreBoard.textContent = score;
+  let sounds = [whack, slap];
+  let randSound = sounds[Math.floor(Math.random() * sounds.length)];
+  randSound.load();
+  randSound.play();
 }
 
-moles.forEach(mole => mole.addEventListener('click', hit));
+moles.forEach(mole => mole.addEventListener("click", hit));
